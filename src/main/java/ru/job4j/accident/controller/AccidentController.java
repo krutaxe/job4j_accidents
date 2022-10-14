689e86a49b285c287d78e6240a6ba9fe4b7ad54b
krutaxe <krutaxe@mail.ru>
2022-10-14 22:31:18 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/ru/job4j/accident/controller/AccidentController.java
+++ src/main/java/ru/job4j/accident/controller/AccidentController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.service.AccidentService;
 
@@ -28,7 +29,7 @@ public class AccidentController {
     }
 
     @PostMapping("/saveAccident")
-    public String save(@ModelAttribute Accident accident) {
+    public String saveAccident(@ModelAttribute Accident accident) {
         accidentService.add(accident);
         return "redirect:/index";
     }
@@ -40,7 +41,21 @@ public class AccidentController {
 
     @PostMapping("/editAccident")
     public String update(@ModelAttribute Accident accident) {
+        accident.setId(accidentService.findById(accident.getId()).getId());
         accidentService.update(accident);
         return "redirect:/accidents";
+    }
+
+    @GetMapping("/formUpdateAccident")
+    public String update(@RequestParam("id") int id, Model model) {
+        model.addAttribute("accident", accidentService.findById(id).getId());
+        System.out.println(accidentService.findById(id).getId());
+        return "accidents";
+    }
+
+    @PostMapping("/updateAccident")
+    public String save(@ModelAttribute Accident accident) {
+        accidentService.add(accident);
+        return "redirect:/formUpdateAccident";
     }
 }
