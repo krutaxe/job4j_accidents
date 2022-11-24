06bfb0e1949bd45a4507e069a1ab8ea31ce6a0b9
krutaxe <krutaxe@mail.ru>
2022-11-24 18:06:12 +0300

    0. Spring DataSource [#6878 #357375]

--- src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
@@ -3,14 +3,15 @@ package ru.job4j.accident.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.model.Rule;
-
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @AllArgsConstructor
@@ -18,15 +19,18 @@ public class AccidentJdbcTemplate {
     private final JdbcTemplate jdbc;
 
     public Accident saveTemp(String[] ids, Accident accident) {
-        jdbc.update("INSERT INTO accident (name, text, address, accident_type_id) "
-                        + "VALUES (?, ?, ?, ?)",
-                accident.getName(),
-                accident.getText(),
-                accident.getAddress(),
-                accident.getType().getId());
-        jdbc.query("SELECT max(id) FROM accident",
-                (RowCallbackHandler) rs ->
-                        accident.setId(rs.getInt("max")));
+        String query = "INSERT INTO accident (name, text, address, accident_type_id)"
+                        + "VALUES (?, ?, ?, ?)";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(con -> {
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, accident.getName());
+            ps.setString(2, accident.getText());
+            ps.setString(3, accident.getAddress());
+            ps.setInt(4, accident.getType().getId());
+            return ps; }, keyHolder);
+
+        accident.setId((Integer) Objects.requireNonNull(keyHolder.getKeys()).get("id"));
         insertRule(ids, accident);
         return accident;
     }
