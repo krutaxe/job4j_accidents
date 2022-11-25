79b08eb7f9bf84b8af059bba8c2e825df9fe29e2
krutaxe <krutaxe@mail.ru>
2022-11-25 18:47:30 +0300

    0. Spring DataSource

--- src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.repository;
 
+
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
