02af0617e41bdacd2949eb42734fc1337239cd91
krutaxe <krutaxe@mail.ru>
2022-11-25 19:24:04 +0300

    test

--- src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
@@ -1,6 +1,5 @@
 package ru.job4j.accident.repository;
 
-
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
