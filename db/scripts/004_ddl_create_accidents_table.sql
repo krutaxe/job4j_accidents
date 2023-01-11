bffaf8b8a980312a9196bcdd6b17b6309184bc8d
krutaxe <krutaxe@mail.ru>
2023-01-11 21:52:53 +0300

    0. Spring test [#6881 #380223]

--- db/scripts/004_ddl_create_accidents_table.sql
+++ db/scripts/004_ddl_create_accidents_table.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS accident (
-  id serial primary key,
-  name text,
-  text text,
-  address text,
-  accident_type_id int references accident_type(id),
-  rule_id int(3)[] references rule(id)
-);
+create TABLE IF NOT EXISTS accident (
+                                        id serial primary key,
+                                        name text,
+                                        text text,
+                                        address text,
+                                        accident_type_id int references accident_type(id),
+                                        rule_id int references rule(id));
